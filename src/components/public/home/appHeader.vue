7bd1bd8257e5961a2b6f9e683cb4f8bed7aef7b3
HOUNKANRIN AYINLA Mouhayad I <40716257+Lino004@users.noreply.github.com>
2021-04-30 13:39:32 +0000

    add sticky

--- src/components/public/home/appHeader.vue
+++ src/components/public/home/appHeader.vue
@@ -36,48 +36,50 @@
           </div>
         </div>
       </div>
-      <div class="bg-white -bottom-2 w-full absolute rounded-2xl flex justify-between px-10">
-        <div class="flex items-center space-x-8 text-lg font-bold text-primary">
-          <div
-            v-for="(item, i) in menu"
-            :key="i"
-            class="relative inline-block dropdown py-10">
-            <router-link
-              :to="{ name: item.to }"
-              tag="a"
-              class="relative">
-              <span class="hover:text-secondary">{{ item.label }}</span>
-            </router-link>
-            <div class="animated fadeIn dropdown-content absolute hidden" v-if="item.children.length">
-              <div class="bg-white rounded-xl p-5 mt-14 space-y-2 flex flex-col">
-                <router-link
-                  v-for="(child, index) in item.children"
-                  :key="index + 'child'"
-                  :to="{ name: child.to }"
-                  tag="a"
-                  class="hover:text-secondary truncate">
-                  {{ child.label }}
-                </router-link>
+      <div class="-bottom-12 w-full absolute">
+        <div class="bg-white rounded-2xl flex justify-between px-10 sticky top-0">
+          <div class="flex items-center space-x-8 text-lg font-bold text-primary">
+            <div
+              v-for="(item, i) in menu"
+              :key="i"
+              class="relative inline-block dropdown py-10">
+              <router-link
+                :to="{ name: item.to }"
+                tag="a"
+                class="relative">
+                <span class="hover:text-secondary">{{ item.label }}</span>
+              </router-link>
+              <div class="animated fadeIn dropdown-content absolute hidden" v-if="item.children.length">
+                <div class="bg-white rounded-xl p-5 mt-14 space-y-2 flex flex-col">
+                  <router-link
+                    v-for="(child, index) in item.children"
+                    :key="index + 'child'"
+                    :to="{ name: child.to }"
+                    tag="a"
+                    class="hover:text-secondary truncate">
+                    {{ child.label }}
+                  </router-link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="flex items-center text-lg space-x-8">
-          <router-link
-            :to="{ name: '' }"
-            tag="a"
-            class="hover:text-secondary text-primary font-bold">
-            SIGN IN
-          </router-link>
-          <button class="hover:bg-secondary
-           bg-primary rounded-2xl font-bold
-           text-white px-5 py-2">
-            APPLY FOR ACCREDITATION
-          </button>
-          <IconCart
-          class="stroke-primary hover:stroke-secondary"
-          width="2.5rem"
-          height="2.375rem"/>
+          <div class="flex items-center text-lg space-x-8">
+            <router-link
+              :to="{ name: '' }"
+              tag="a"
+              class="hover:text-secondary text-primary font-bold">
+              SIGN IN
+            </router-link>
+            <button class="hover:bg-secondary
+            bg-primary rounded-2xl font-bold
+            text-white px-5 py-2">
+              APPLY FOR ACCREDITATION
+            </button>
+            <IconCart
+            class="stroke-primary hover:stroke-secondary"
+            width="2.5rem"
+            height="2.375rem"/>
+          </div>
         </div>
       </div>
     </div>
