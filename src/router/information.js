fa81ceed2adefbe7d59fe1036b2114958b5f56cd
Makloud <mouhamedayinla3@gmail.com>
2021-05-10 17:22:49 +0100

    before push

--- src/router/information.js
+++ src/router/information.js
@@ -1,10 +1,5 @@
 export default [
   {
-    path: '/informations',
-    name: 'informations',
-    component: () => import('@/views/public/Informations/informations.vue')
-  },
-  {
     path: '/terms',
     name: 'terms',
     component: () => import('@/views/public/Informations/Terms.vue')
