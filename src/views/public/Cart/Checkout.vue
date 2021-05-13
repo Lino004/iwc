5be9993d41b213041c13369689cade7240eec4ae
Makloud <mouhamedayinla3@gmail.com>
2021-05-13 16:59:33 +0100

    ...

--- src/views/public/Cart/Checkout.vue
+++ src/views/public/Cart/Checkout.vue
@@ -70,6 +70,57 @@
             />
         </form>
       </div>
+
+      <div class="border-t-3 border-b-3 border-grid7 py-6 pl-20">
+        SHIPPING INFORMATION
+      </div>
+      <div class="py-16">
+        <form action="" class="flex flex-wrap justify-between items-center">
+          <input
+              type="text"
+              id="firstName"
+              v-model="firstName"
+              placeholder="First Name"
+              class="input-about border-none bg-grid3 rounded-15px"
+            />
+
+            <input
+              type="text"
+              id="lastName"
+              v-model="lastName"
+              placeholder="Last Name"
+              class="input-about border-none bg-grid3 rounded-15px"
+            />
+
+          <input
+              type="email"
+              id="email"
+              v-model="valueEmail"
+              placeholder="Email Address"
+              class="input-about border-none bg-grid3 rounded-15px"
+            />
+
+          <input
+              type="number"
+              id="phone"
+              v-model="phone"
+              placeholder="Contact Number"
+              class="input-about border-none bg-grid3 rounded-15px"
+            />
+
+          <input
+              type="text"
+              id="address"
+              v-model="address"
+              placeholder="Address"
+              class="input-about border-none bg-grid3 rounded-15px"
+            />
+
+          <div class="w-5/12">
+            <input type="checkbox" id="check" v-model="check"> <label for="check">Same as billing address</label>
+          </div>
+        </form>
+      </div>
     </div>
   </section>
 </template>
