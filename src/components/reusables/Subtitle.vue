926ab1c21040e3c9b9df5b847a2e1c0da981a958
Makloud <mouhamedayinla3@gmail.com>
2021-05-23 12:01:44 +0100

    subtitle component created

---
+++ src/components/reusables/Subtitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-primary font-bold border-t-3 border-b-3 border-grid7 py-6 pl-20">
+      {{ subtitle }}
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subtitle: String
+  }
+}
+</script>
