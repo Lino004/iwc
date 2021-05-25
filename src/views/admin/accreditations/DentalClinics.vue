b71b23db10d37546f7c259dde4c987419219c0c2
Makloud <mouhamedayinla3@gmail.com>
2021-05-25 16:48:00 +0100

    dental clinics done

--- src/views/admin/accreditations/DentalClinics.vue
+++ src/views/admin/accreditations/DentalClinics.vue
@@ -3,11 +3,136 @@
     <h3 class="title-page-admin">
       Dental Clinics
     </h3>
+
+    <div class="text-right space-x-4 my-10">
+      <button class="iwc-btn-action w-auto bg-secondary px-8"> ADD PRODUCT </button>
+      <button class="iwc-btn-action bg-grid1 w-auto px-8"> ACTION </button>
+    </div>
+
+    <ActionsTable
+      :data-filter="['All']"
+      :data-sort-by="['Ascending']"
+    />
+
+    <div>
+      <div class="flex flex-row justify-end gap-10 mt-5">
+        <p class="underline">Upload..</p>
+        <p class="underline">Generate Report</p>
+      </div>
+
+      <div>
+        <table-admin
+          :columns="columns"
+          :data="products"
+          checkable
+          children
+          children-key="id">
+          <template #children="">
+            <td colspan="2" class="py-5 px-3 border-l-8 border-primary"></td>
+            <td colspan="4" class="py-5 px-3">
+              <p class="text-lg">
+                <span class="font-bold ">Address:</span>
+                1234 Happy Street | City Province
+              </p>
+              <p class="text-lg py-11">
+                <span class="font-bold ">Contact Email:</span>
+                mail@dentist.com
+              </p>
+              <p class="text-lg">
+                <span class="font-bold ">Schedule:</span>
+                MON to FRI | 9AM-5PM
+              </p>
+            </td>
+          </template>
+        </table-admin>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      filter: 0,
+      sortBy: 0,
+      columns: [
+        {
+          field: 'id',
+          label: 'ID',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'clinic',
+          label: 'Clinic',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'contactPerson',
+          label: 'Contact Person',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'contactNumber',
+          label: 'Contact Number',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'status',
+          label: 'Status',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        }
+      ],
+      products: [
+        {
+          id: '1',
+          clinic: 'Boyet Clinic',
+          contactPerson: 'Boyet Fernandez',
+          contactNumber: '123-456-7891',
+          status: 'Active'
+        },
+        {
+          id: '2',
+          clinic: 'Boyet Clinic',
+          contactPerson: 'Boyet Fernandez',
+          contactNumber: '123-456-7891',
+          status: 'Active'
+        },
+        {
+          id: '3',
+          clinic: 'Boyet Clinic',
+          contactPerson: 'Boyet Fernandez',
+          contactNumber: '123-456-7891',
+          status: 'Active'
+        },
+        {
+          id: '4',
+          clinic: 'Boyet Clinic',
+          contactPerson: 'Boyet Fernandez',
+          contactNumber: '123-456-7891',
+          status: 'Active'
+        },
+        {
+          id: '5',
+          clinic: 'Boyet Clinic',
+          contactPerson: 'Boyet Fernandez',
+          contactNumber: '123-456-7891',
+          status: 'Active'
+        },
+        {
+          id: '6',
+          clinic: 'Boyet Clinic',
+          contactPerson: 'Boyet Fernandez',
+          contactNumber: '123-456-7891',
+          status: 'Active'
+        }
+      ]
+    }
+  }
 }
 </script>
