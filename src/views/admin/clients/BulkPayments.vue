ec31843731b7aaf06765ecf488ca0239856eb616
Makloud <mouhamedayinla3@gmail.com>
2021-05-25 20:55:13 +0100

    bulk payments done

--- src/views/admin/clients/BulkPayments.vue
+++ src/views/admin/clients/BulkPayments.vue
@@ -5,19 +5,97 @@
     </h3>
 
     <div class="text-right space-x-4 my-10">
-      <button class="iwc-btn-action w-auto bg-secondary px-8"> ADD PRODUCT </button>
-      <button class="iwc-btn-action bg-grid1 w-auto px-8"> ACTION </button>
+      <button class="iwc-btn-action w-auto bg-secondary px-8">UPLOAD</button>
+      <button class="iwc-btn-action bg-grid1 w-auto px-8">ACTION</button>
     </div>
 
     <ActionsTable
       :data-filter="['All']"
       :data-sort-by="['Ascending']"
     />
+
+    <div>
+      <table-admin
+        :columns="columns"
+        :data="procedures"
+        checkable
+      >
+      </table-admin>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      filter: 0,
+      sortBy: 0,
+      columns: [
+        {
+          field: 'id',
+          label: 'ID',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'dateUploaded',
+          label: 'Date Uploaded',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'uploader',
+          label: 'Uploader',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'download',
+          label: ' ',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        }
+      ],
+      procedures: [
+        {
+          id: '1',
+          dateUploaded: '2/2/2021',
+          uploader: 'Boyet Fernandez',
+          download: 'Download.csv'
+        },
+        {
+          id: '2',
+          dateUploaded: '2/2/2021',
+          uploader: 'Boyet Fernandez',
+          download: 'Download.csv'
+        },
+        {
+          id: '3',
+          dateUploaded: '2/2/2021',
+          uploader: 'Boyet Fernandez',
+          download: 'Download.csv'
+        },
+        {
+          id: '4',
+          dateUploaded: '2/2/2021',
+          uploader: 'Boyet Fernandez',
+          download: 'Download.csv'
+        },
+        {
+          id: '5',
+          dateUploaded: '2/2/2021',
+          uploader: 'Boyet Fernandez',
+          download: 'Download.csv'
+        },
+        {
+          id: '6',
+          dateUploaded: '2/2/2021',
+          uploader: 'Boyet Fernandez',
+          download: 'Download.csv'
+        }
+      ]
+    }
+  }
 }
 </script>
