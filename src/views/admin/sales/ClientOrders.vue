657a95de2eb47fe216c6c3b93e85e7c33ad9f9ed
Makloud <mouhamedayinla3@gmail.com>
2021-05-25 15:13:27 +0100

    prise en main

--- src/views/admin/sales/ClientOrders.vue
+++ src/views/admin/sales/ClientOrders.vue
@@ -3,6 +3,11 @@
     <h3 class="title-page-admin">
       Client Orders
     </h3>
+
+    <div class="text-right space-x-4 my-10">
+      <button class="iwc-btn-action w-auto bg-secondary px-8"> ADD PRODUCT </button>
+      <button class="iwc-btn-action bg-grid1 w-auto px-8"> ACTION </button>
+    </div>
   </section>
 </template>
 
