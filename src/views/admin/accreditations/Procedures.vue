9dc410b03c4edcb7ae23807eccb120d64bfab329
Makloud <mouhamedayinla3@gmail.com>
2021-05-25 16:24:07 +0100

    procedures done

--- src/views/admin/accreditations/Procedures.vue
+++ src/views/admin/accreditations/Procedures.vue
@@ -3,11 +3,100 @@
     <h3 class="title-page-admin">
       Procedures
     </h3>
+
+    <div class="text-right space-x-4 my-10">
+      <button class="iwc-btn-action w-auto bg-secondary px-8"> ADD PRODUCT </button>
+      <button class="iwc-btn-action bg-grid1 w-auto px-8"> ACTION </button>
+    </div>
+
+    <ActionsTable
+      :data-filter="['All']"
+      :data-sort-by="['Ascending']"
+    />
+
+    <div>
+      <table-admin
+        :columns="columns"
+        :data="clients"
+        checkable
+      >
+      </table-admin>
+    </div>
+
   </section>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      filter: 0,
+      sortBy: 0,
+      columns: [
+        {
+          field: 'id',
+          label: 'ID',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'name',
+          label: 'Name',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'abbreviation',
+          label: 'Abbreviation',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'variableName',
+          label: 'Variable Name',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        }
+      ],
+      clients: [
+        {
+          id: '1',
+          name: 'Temporary Filling',
+          abbreviation: 'TF',
+          variableName: 'Teeth'
+        },
+        {
+          id: '2',
+          name: 'Consultation',
+          abbreviation: 'Cons',
+          variableName: 'Teeth'
+        },
+        {
+          id: '3',
+          name: 'Treatment of Wounds, Lesions, and Burns',
+          abbreviation: 'TWLB',
+          variableName: 'Teeth'
+        },
+        {
+          id: '4',
+          name: 'Light-Curing/Permanent Filling',
+          abbreviation: 'LPF',
+          variableName: 'Teeth'
+        },
+        {
+          id: '5',
+          name: 'Adjustment of Dentures',
+          abbreviation: 'AD',
+          variableName: 'Teeth'
+        },
+        {
+          id: '6',
+          name: 'Simple Tooth Extraction',
+          abbreviation: 'STE',
+          variableName: 'Teeth'
+        }
+      ]
+    }
+  }
 }
 </script>
