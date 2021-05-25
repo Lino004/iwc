443555469a77f01bbdafa78ac4e8283a131cbb45
Makloud <mouhamedayinla3@gmail.com>
2021-05-25 15:28:10 +0100

    clientOrders created

--- src/views/admin/sales/ClientOrders.vue
+++ src/views/admin/sales/ClientOrders.vue
@@ -8,11 +8,130 @@
       <button class="iwc-btn-action w-auto bg-secondary px-8"> ADD PRODUCT </button>
       <button class="iwc-btn-action bg-grid1 w-auto px-8"> ACTION </button>
     </div>
+
+    <ActionsTable
+      :data-filter="['All']"
+      :data-sort-by="['Ascending']"
+    />
+
+    <div>
+      <table-admin
+        :columns="columns"
+        :data="clients"
+        checkable
+        >
+      </table-admin>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      filter: 0,
+      sortBy: 0,
+      columns: [
+        {
+          field: 'id',
+          label: 'ID',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'client',
+          label: 'Client',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'status',
+          label: 'Status',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'paymentMethod',
+          label: 'Payment Method',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'totalOrder',
+          label: 'Total Order',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'dateOrdered',
+          label: 'Date Ordered',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        },
+        {
+          field: 'dateReceived',
+          label: 'Date Received',
+          class: 'text-left text-lg',
+          cellClass: 'text-lg font-bold'
+        }
+      ],
+      clients: [
+        {
+          id: '1',
+          client: 'Boyet',
+          status: 'Completed',
+          paymentMethod: 'Credit Card',
+          totalOrder: '1000',
+          dateOrdered: '4 March 2021',
+          dateReceived: '4 March 2021'
+        },
+        {
+          id: '2',
+          client: 'Boyet',
+          status: 'Completed',
+          paymentMethod: 'Credit Card',
+          totalOrder: '1000',
+          dateOrdered: '4 March 2021',
+          dateReceived: '4 March 2021'
+        },
+        {
+          id: '3',
+          client: 'Boyet',
+          status: 'Completed',
+          paymentMethod: 'Credit Card',
+          totalOrder: '1000',
+          dateOrdered: '4 March 2021',
+          dateReceived: '4 March 2021'
+        },
+        {
+          id: '4',
+          client: 'Boyet',
+          status: 'Completed',
+          paymentMethod: 'Credit Card',
+          totalOrder: '1000',
+          dateOrdered: '4 March 2021',
+          dateReceived: '4 March 2021'
+        },
+        {
+          id: '5',
+          client: 'Boyet',
+          status: 'Completed',
+          paymentMethod: 'Credit Card',
+          totalOrder: '1000',
+          dateOrdered: '4 March 2021',
+          dateReceived: '4 March 2021'
+        },
+        {
+          id: '6',
+          client: 'Boyet',
+          status: 'Completed',
+          paymentMethod: 'Credit Card',
+          totalOrder: '1000',
+          dateOrdered: '4 March 2021',
+          dateReceived: '4 March 2021'
+        }
+      ]
+    }
+  }
 }
 </script>
